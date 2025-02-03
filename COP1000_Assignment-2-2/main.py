93337bf9740e5f324b44f9aac8313fde491f56e4
x.mat1 <mat12345duq6789@gmail.com>
2025-02-02 21:28:00 -0500

    assigned variables

---
+++ COP1000_Assignment-2-2/main.py
@@ -0,0 +1,11 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Create your variables here
+myCurrentAge = 20
+currentYear = 2025
+
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
